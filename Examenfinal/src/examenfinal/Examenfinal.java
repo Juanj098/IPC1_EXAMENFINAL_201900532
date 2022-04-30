271668db46950443dcdf863aa5eb44615618e4f4
juanjo <juanjgerardiher1998.com>
2022-04-29 22:58:04 -0600

    final :c

--- Examenfinal/src/examenfinal/Examenfinal.java
+++ Examenfinal/src/examenfinal/Examenfinal.java
@@ -18,28 +18,62 @@ public class Examenfinal {
         //menu
         Scanner opcion = new Scanner(System.in);
         int opc = 0;
-        System.out.println("--------------------");
-        System.out.println("    EXAMEN FINAL    ");
-        System.out.println("--------------------");
-        System.out.println("1.  Problema no.1   ");
-        System.out.println("2.  Problema no.2   ");
-        System.out.println("3.  Problema no.3   ");
-        System.out.println("--------------------");
-        System.out.println("Ingrese no. de problema: ");
-        opc = opcion.nextInt();
-        switch(opc){
+        do{
+            System.out.println("--------------------");
+            System.out.println("    EXAMEN FINAL    ");
+            System.out.println("--------------------");
+            System.out.println("1.  Problema no.1   ");
+            System.out.println("2.  Problema no.2   ");
+            System.out.println("3.  Problema no.3   ");
+            System.out.println("--------------------");
+            System.out.println("Ingrese no. de problema: ");
+            opc = opcion.nextInt();
+            switch(opc){
             case 1: //problema 1
-                System.out.println("Problema 1");
-            break;
+                Scanner num = new Scanner(System.in);
+                int num1 =0;
+                int num2=0;
+                System.out.println("Ingrese numero 1: ");
+                num1 = num.nextInt();
+                System.out.println("Ingrese numero 2: ");
+                num2 = num.nextInt();
+                if(num1>num2){
+                    System.out.println(num1+" es mayor");
+                }else{
+                    System.out.println(num2+" es mayor");
+                }
+                break;
             case 2: //problema 2
+                Scanner numi = new Scanner (System.in);
+                System.out.println("Ingresa numero: ");
+                int numm = numi.nextInt();
+                if (numm%2 == 1){
+                    char matriz[][] = new char[numm][numm];
+                    for(int x=0;x<matriz.length;x++){
+                        for(int y=0; y<matriz[0].length;y++){
+                            if((y+x)>=numm/2 && (y-x)<=numm/2 && (x%2)==1){
+                                matriz[x][y]='*';
+                            }else{
+                                matriz[x][y]=' ';
+                            }
+                            System.out.print(" "+matriz[x][y]+" ");
+                        }
+                        System.out.println(" ");
+                    }
+                                              
+                }else{
+                    System.out.println("EL NUMERO INGRESADO DEBE SER UN NUMERO IMPAR");
+                }
                 System.out.println("Problema 2");
-            break;
+                break;
             case 3: //problema 3
                 System.out.println("Problema 3");
-            break;
-            default: //numero mayor a 3
-            System.out.println("Opcion no disponible");
-        }
+                break;
+                default: //numero mayor a 3
+                System.out.println("Opcion no disponible");
+            }     
+        }while(opc!=4);
+
     }
     
 }
